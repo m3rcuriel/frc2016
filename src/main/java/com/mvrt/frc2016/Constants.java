9499e2e7e5d874119e5ff17f60558e1a594dce9d
Lee Mracek <lee.mracek@gmail.com>
2016-01-12 20:53:26 -0800

    Create default Constants.java and Robot.java
    Also migrated ConstantsBase.java from lib, as lib will be populated
    as the season progresses, rather than all at once. Currently Robot
    has the Scheduler and RobotState accessors.

---
+++ src/main/java/com/mvrt/frc2016/Constants.java
@@ -0,0 +1,25 @@
+package com.mvrt.frc2016;
+
+import com.mvrt.lib.ConstantsBase;
+
+/**
+ * A file holding public static constants for the robot.
+ *
+ * @author Lee Mracek
+ */
+public class Constants extends ConstantsBase {
+
+    /**
+     * Allows the programmer to customize the location of the constants save file.
+     *
+     * @return the string path of the constants file
+     */
+    @Override
+    public String getFileLocation() {
+        return "~/constants.txt";
+    }
+
+    static {
+        new Constants().loadFromFile();
+    }
+}
---
+++ src/main/java/com/mvrt/frc2016/Robot.java
@@ -0,0 +1,84 @@
+package com.mvrt.frc2016;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+/**
+ * Base class for the robot. This is the class which will run when the code is initialized on the RoboRIO. Many optional
+ * methods can be overridden in addition to the periodic methods.
+ *
+ * @author Lee Mracek
+ */
+public class Robot extends IterativeRobot {
+
+    public enum RobotState {
+        DISABLED, AUTONOMOUS, TELEOP;
+    }
+
+    private static RobotState robotState = RobotState.DISABLED;
+
+    /**
+     * Retrieves the current enum state of the robot.
+     *
+     * @return the RobotState
+     */
+    public static RobotState getState() {
+        return robotState;
+    }
+
+    /**
+     * Runs when the robot is initially turned on.
+     */
+    @Override
+    public void robotInit() {
+
+    }
+
+    /**
+     * Runs once when the robot enters autonomous.
+     */
+    @Override
+    public void autonomousInit() {
+        robotState = RobotState.AUTONOMOUS;
+    }
+
+    /**
+     * Loops every approximately 20 ms during the autonomous period.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    /**
+     * Runs once when the robot enteres teleop.
+     */
+    @Override
+    public void teleopInit() {
+        robotState = RobotState.TELEOP;
+    }
+
+    /**
+     * Loops every approximately 20 ms during the teleoperated period.
+     */
+    @Override
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    /**
+     * Runs whenever the robot enters disabled.
+     */
+    @Override
+    public void disabledInit() {
+        robotState = RobotState.DISABLED;
+    }
+
+    /**
+     * Loops every approximately 20 ms during the disabled period.
+     */
+    @Override
+    public void disabledPeriodic() {
+        Scheduler.getInstance().run();
+    }
+}
