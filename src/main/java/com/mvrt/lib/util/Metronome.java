83d81c896341a50936be06482635a683ee5d20e8
Lee Mracek <lee.mracek@gmail.com>
2016-01-18 02:02:50 -0800

    Add Conductor to handle looping Runnables

--- src/main/java/com/mvrt/lib/util/Metronome.java
+++ src/main/java/com/mvrt/lib/util/Metronome.java
@@ -5,6 +5,8 @@ import com.mvrt.lib.components.Clock;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * A class which 'ticks' at a given rate.
+ *
  * @author Lee Mracek
  */
 public interface Metronome {
@@ -18,13 +20,13 @@ public interface Metronome {
 
   /**
    * Construct a new Metronome using the 'busy' method.
-   *
+   * <p>
    * Essentially, an empty while loop holds thread context and forces the wait.
    *
    * @param period the period at which the metronome ticks
-   * @param unit the {@link TimeUnit} of the period
-   * @param time the Clock used for the metronome
-   * @return
+   * @param unit   the {@link TimeUnit} of the period
+   * @param time   the Clock used for the metronome
+   * @return the constructed Metronome
    */
   static Metronome metronome(long period, TimeUnit unit, Clock time) {
     long periodInNanos = unit.toNanos(period);
